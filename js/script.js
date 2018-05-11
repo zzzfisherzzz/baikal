088c0e0d9299ce9f987a60bc44091341c514c381
Artem <zzzfisherzzz@bigmir.net>
2018-05-11 15:26:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(function(){
+
+	"use strict";
+
+	$('.title_box').click(function(){
+
+		$(this).toggleClass('open');
+		$(this).next('div.list_link').toggleClass('open');
+    
+
+	});
+    
+    $('#slider_price').slider({
+  		max: 1000, 
+  		min: 0,
+  		range: true,
+  		values: [190,728],
+  		slide: function( event, ui ) {
+  			$('input[name="minPrice"]').val( '$' + ui.values[0] );
+  			$('input[name="maxPrice"]').val( '$' + ui.values[1] );
+  		}
+	});
+
+	$('input[name="minPrice"]').val( '$' + $('#slider_price').slider('values',0) );
+	$('input[name="maxPrice"]').val( '$' + $('#slider_price').slider('values',1) );
+
+
+  $('#cart, .title_cart').click(function(){
+    $('#cart_box').toggleClass('open');
+  });
+
+  $('#sign').click(function(){
+    $('#black_fill').toggleClass('open');
+    $('#modal').toggleClass('open');
+  });
+
+  $('#close_modal, #black_fill').click(function(){
+    $('#black_fill').toggleClass('open');
+    $('#modal').toggleClass('open');
+  });
+
+
+  $('#search_button').click(function(){
+    $('#search_panel').toggleClass('open');
+  });
+
+  $('#load_more').click(function(event){
+    event.preventDefault();
+    $('.list_product').append(' 1 <img src="img/image3.png" alt="product">');
+  });
+
+
+
+
+});
+
+
+
+
+
+
+
